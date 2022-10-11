82784245ab8bce7efdb95b102d1a2651b66d1e13
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-10-12 00:09:03 +0100

    p

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length = 50)
     description = models.TextField()
     quantity = models.IntegerField()
